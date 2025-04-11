a2e729a120f330b06ae5f783ee111671094f7d07
PauloHGrande <pauloav2013@gmail.com>
2025-04-11 17:20:00 -0300

    Criando a Entity de order-pedidos

---
+++ order/src/main/java/com/ms/order/model/Order.java
@@ -0,0 +1,26 @@
+package com.ms.order.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PEDIDO")
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;  // Melhor para evitar conflito de id´s devido ser um microservice
+	
+    private String product;
+    private Double price;
+    private String status;
+    
+}
